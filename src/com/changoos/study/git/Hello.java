846b430f9bc3eba95fb10f39920b5428f1ed3bac
cglee079 <cglee079@gmail.com>
2018-12-07 17:12:56 +0900

    Second Commit, Hello.java modified

--- src/com/changoos/study/git/Hello.java
+++ src/com/changoos/study/git/Hello.java
@@ -3,5 +3,6 @@ package com.changoos.study.git;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("I'm modfied...!");
 	}
 }
